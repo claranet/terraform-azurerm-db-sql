bf2fe759ee9a443815cc38c586feab51ba6e9554
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2019-04-12 09:52:24 +0200

    AZ-70 Add connection string output

--- outputs.tf
+++ outputs.tf
@@ -27,3 +27,9 @@ output "sql_databases_default_secondary_location" {
   description = "The default secondary location of the SQL Databases"
   value       = "${azurerm_sql_database.db.*.default_secondary_location}"
 }
+
+output "default_administrator_databases_connection_strings" {
+  description = "Connection strings of the SQL Databases with administrator credentials"
+  value       = "${formatlist("Server=tcp:%s;Database=%s;User ID=%s;Password=%s;Encrypt=true;", azurerm_sql_server.server.fully_qualified_domain_name, azurerm_sql_database.db.*.name, var.administrator_login, var.administrator_password)}"
+  sensitive   = true
+}
