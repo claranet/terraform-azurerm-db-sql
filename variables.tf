9f3077360eb99601b3bc87f6b6e52e4c9003f35a
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2019-10-16 11:13:12 +0200

    AZ-133: Use 3rd party logging module

--- variables.tf
+++ variables.tf
@@ -132,16 +132,10 @@ variable "databases_collation" {
   default     = "SQL_LATIN1_GENERAL_CP1_CI_AS"
 }
 
-variable "enable_logs_to_storage" {
-  description = "Boolean flag to specify whether the logs should be sent to the Storage Account"
-  type        = string
-  default     = "false"
-}
-
-variable "enable_logs_to_log_analytics" {
-  description = "Boolean flag to specify whether the logs should be sent to Log Analytics"
-  type        = string
-  default     = "false"
+variable "enable_logging" {
+  description = "Boolean flag to specify whether logging is enabled"
+  type        = bool
+  default     = true
 }
 
 variable "logs_storage_retention" {
