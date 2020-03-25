e53335ccdf8f7dc0a5750d02c9ecbd776711a9df
Jérôme Respaut <jerome.respaut@fr.clara.net>
2020-03-25 14:47:23 +0100

    AZ-133: Use HCL2 splat expression

--- r-logs.tf
+++ r-logs.tf
@@ -2,7 +2,7 @@ module "logging" {
   source = "github.com/claranet/terraform-azurerm-diagnostic-settings.git?ref=multiple-resources"
 
   resources_count = var.enable_logging ? length(var.databases_names) : 0
-  resource_ids    = azurerm_sql_database.db.*.id
+  resource_ids    = azurerm_sql_database.db[*].id
   retention_days  = var.logs_storage_retention
 
   storage_account_id         = var.logs_storage_account_id
