af4cf1a61ccc42bba45cff96c036862055a69a01
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2020-10-21 16:54:00 +0200

    AZ-273: Add diagnostic settings on Elastic Pool

--- r-logs.tf
+++ r-logs.tf
@@ -1,8 +1,17 @@
-module "logging" {
+module "db_logging" {
   for_each = var.logs_destinations_ids != [] ? toset(var.databases_names) : toset([])
 
   source = "git::ssh://git@git.fr.clara.net/claranet/projects/cloud/azure/terraform/modules/diagnostic-settings.git?ref=AZ-160-revamp"
 
   resource_id           = azurerm_sql_database.db[each.key].id
+  logs_destinations_ids = var.logs_destinations_ids
+}
+
+module "pool_logging" {
+  count = var.logs_destinations_ids != [] ? 1 : 0
+
+  source = "git::ssh://git@git.fr.clara.net/claranet/projects/cloud/azure/terraform/modules/diagnostic-settings.git?ref=AZ-160-revamp"
+
+  resource_id           = azurerm_mssql_elasticpool.elastic_pool.id
   logs_destinations_ids = var.logs_destinations_ids
 }
