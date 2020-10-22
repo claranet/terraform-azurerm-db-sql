52ba0b6c8e0db18885185c88673a43dde1692702
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2020-10-22 10:06:25 +0200

    AZ-273: Use full objects as outputs

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,16 @@
-output "sql_server_id" {
-  description = "Id of the SQL Server"
-  value       = azurerm_sql_server.server.id
+output "sql_server" {
+  description = "SQL Server"
+  value       = azurerm_sql_server.server
 }
 
-output "sql_server_fqdn" {
-  description = "Fully qualified domain name of the SQL Server"
-  value       = azurerm_sql_server.server.fully_qualified_domain_name
+output "sql_elastic_pool" {
+  description = "SQL Elastic Pool"
+  value       = azurerm_mssql_elasticpool.elastic_pool
 }
 
-output "sql_elastic_pool_id" {
-  description = "Id of the SQL Elastic Pool"
-  value       = azurerm_mssql_elasticpool.elastic_pool.id
-}
-
-output "sql_databases_id" {
-  description = "Map of the SQL Databases IDs"
-  value       = { for db in azurerm_sql_database.db : db.name => db.id }
-}
-
-output "sql_databases_creation_date" {
-  description = "Map of the SQL Databases creation dates"
-  value       = { for db in azurerm_sql_database.db : db.name => db.creation_date }
-}
-
-output "sql_databases_default_secondary_location" {
-  description = "Map of the SQL Databases default secondary location"
-  value       = { for db in azurerm_sql_database.db : db.name => db.default_secondary_location }
+output "sql_databases" {
+  description = "SQL Databases"
+  value       = azurerm_sql_database.db
 }
 
 output "default_administrator_databases_connection_strings" {
@@ -45,7 +30,6 @@ output "default_administrator_databases_connection_strings" {
 output "databases_users" {
   description = "Map of the SQL Databases dedicated usernames"
   value       = local.databases_users
-  sensitive   = true
 }
 
 output "databases_users_passwords" {
